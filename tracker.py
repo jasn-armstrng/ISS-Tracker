bbf3648581025f7fbf8a59cbb55451a12b7dde9f
Jason Armstrong <jasnarmstrng@gmail.com>
2024-01-05 23:17:37 -0400

    feat: Add parsing function and supporting dataclass

--- tracker.py
+++ tracker.py
@@ -1,21 +1,25 @@
 import json
 import requests
-import tzlocal
 from datetime import datetime
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass
+class WhereIsISSOver:
+    """Class for <to complete meaningfully>"""
+    timestamp: str
+    latitude: str
+    longitude: str
 
 
 def get_data_from_api(url: str, api_key: str=None) -> json:
+    """Returns json object result of API data fetch"""
     try:
         request = requests.get(url)
-        # process the specific codes from the range 400-599
-        # that you are interested in first
-        if request.status_code == 400:
+        if request.status_code >= 400:
             invalid_request_reason = request.text
             print(f"Your request has failed because: {invalid_request_reason}")
-            return
-        # this will handle all other errors
-        elif request.status_code > 400:
-            print(f"Your request has failed with status code: {request.status_code}")
             return
     except requests.exceptions.ConnectionError as err:
         raise SystemExit(err)
@@ -23,15 +27,28 @@ def get_data_from_api(url: str, api_key: str=None) -> json:
     return request.json()
 
 
-def unixtime_to_date(timestamp: int) -> datetime:
+def unixtime_to_date(timestamp: int) -> str:
+    """Retuns UTC time from UNIX timestamp"""
     utc_time = datetime.utcfromtimestamp(timestamp)
     return utc_time.strftime("%Y-%m-%d %H:%M:%S+00:00 (UTC)")
+
+
+def parse_iss_now_data(data: dict) -> Any:
+    """Returns a object with partial details of the ISS's position"""
+    timestamp = data.get("timestamp", None)
+    latitude  = data.get("iss_position", None).get("latitude", None)
+    longitude = data.get("iss_position", None).get("longitude", None)
+    record = WhereIsISSOver(unixtime_to_date(timestamp), latitude, longitude)
+    return record
 
 
 if __name__ == "__main__":
     ISS_NOW_URL = "http://api.open-notify.org/iss-now.json"
     iss_now = get_data_from_api(ISS_NOW_URL)
-    timestamp = iss_now.get("timestamp", None)
-    latitude  = iss_now.get("iss_position", None).get("latitude", None)
-    longitude = iss_now.get("iss_position", None).get("longitude", None)
-    print(f"{unixtime_to_date(timestamp)}: {latitude}, {longitude}")
+    where_is_iss = parse_iss_now_data(iss_now)
+    # Next:
+    # • Pass where_is_iss lat, long to reverse-geolocating function
+    # • Identify the addressing fields to be used from results returned
+    # • Complete speciification of WhereIsISSOver
+    # • Pass where_is_iss lat, long to reverse-geolocating function
+    #   and update WhereIsISSOver
