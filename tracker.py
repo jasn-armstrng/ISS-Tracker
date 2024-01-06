d7153f89744cba50de2828e0b69f8cc39adfd148
Jason Armstrong <jasnarmstrng@gmail.com>
2024-01-06 13:29:15 -0400

    refactor: Add error handling

--- tracker.py
+++ tracker.py
@@ -20,60 +20,67 @@ REV_GEO_URL = config["Geoapify"]["url"]
 REV_GEO_KEY = config["Geoapify"]["key"]
 
 
-def get_data_from_api(url: str, api_key: str=None) -> json:
-    """Returns json object result of API data fetch"""
+def get_data_from_api(url: str) -> json:
+    """Returns json object result of API data fetch or logs an error"""
     try:
-        request = requests.get(url)
-        if request.status_code >= 400:
-            invalid_request_reason = request.text
-            logging.error(f"Your request has failed because: {invalid_request_reason}")
-            return
+        response = requests.get(url)
+        if response.status_code >= 400:
+            invalid_request_reason = response.text
+            print(f"Your request has failed because: {invalid_request_reason}")
+            return {}
+        else:
+            return response.json()
     except requests.exceptions.ConnectionError as err:
-        raise SystemExit(err)
-
-    return request.json()
+        logging.error(f"Your request has failed because: {err}")
 
 
 def unixtime_to_date(timestamp: int) -> str:
-    """Retuns UTC time from UNIX timestamp"""
-    utc_time = datetime.utcfromtimestamp(timestamp)
-    return utc_time.strftime("%Y-%m-%d %H:%M:%S+00:00 (UTC)")
+    """Converts UNIX timestamp to UTC time in the format YYYY-MM-DD HH:MM:SS+00:00 (UTC)"""
+    return datetime.utcfromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S+00:00 (UTC)")
 
 
-def parse_iss_now_data(data: json) -> dict:
-    """Returns a object with partial details of the ISS's position"""
-    timestamp = data.get("timestamp", None)
-    latitude  = data.get("iss_position", None).get("latitude", None)
-    longitude = data.get("iss_position", None).get("longitude", None)
-    return {'timestamp': unixtime_to_date(timestamp), 'latitude': latitude, 'longitude': longitude}
+def parse_iss_data(data: json) -> dict:
+    """Extracts and returns ISS position (lat, long) and timestamp"""
+    try:
+        timestamp = data["timestamp"]
+        position = data["iss_position"]
+        return {
+            'timestamp': unixtime_to_date(timestamp),
+            'latitude': position["latitude"],
+            'longitude': position["longitude"]
+        }
+    except KeyError:
+        logging.error("Invalid ISS data format")
+        return {}
 
 
 def reverse_geolocate(lat: str, long: str) -> json:
-    """Return reverse geolocation details in JSON"""
+    """Return reverse geolocation details"""
     URL = f"{REV_GEO_URL}?lat={lat}&lon={long}&apiKey={REV_GEO_KEY}"
-    location = get_data_from_api(URL)
-    return location
+    return get_data_from_api(URL)
 
 
-def parse_revgeo_data(data: json) -> str:
-    """Returns address formatted address from reverse geolocation"""
-    address = data.get("features", None)[0].get("properties", None).get("formatted", None)
-    return address
+def get_formatted_address(data: json) -> str:
+    """Extracts and returns formatted address from geolocation data"""
+    try:
+        return data["features"][0]["properties"]["formatted"]
+    except (KeyError, IndexError):
+        logging.error("Invalid geolocation data format")
+        return ""
 
 
-if __name__ == "__main__":
-    iss_now = get_data_from_api(ISS_NOW_URL)
-    where_is_iss = parse_iss_now_data(iss_now)
-    # pprint(where_is_iss)
-    # Next:
-    # • Pass where_is_iss lat, long to reverse-geolocating function
-    addressing = reverse_geolocate(where_is_iss['latitude'], where_is_iss['longitude'])  # This can fail if ISS now has missing data
-    # • Identify the addressing fields to be used from results returned
-    # pprint(addressing)
-    # • Complete speciification of WhereIsISSOver
-    # • Pass where_is_iss lat, long to reverse geolocation parser
-    address = parse_revgeo_data(addressing)
-    # • Update where_is_iss
-    where_is_iss['address'] = address
-    logging.info(where_is_iss)
-    # -> {'timestamp': '2024-01-06 04:39:44+00:00 (UTC)', 'latitude': '-11.4986', 'longitude': '57.0169', 'address': 'Indian Ocean'}
+def iss_position() -> None:
+    iss_data = get_data_from_api(ISS_NOW_URL)
+    if iss_data:
+        _iss_position = parse_iss_data(iss_data)
+        if iss_position:
+            geo_data = reverse_geolocate(_iss_position['latitude'], _iss_position['longitude'])
+            address = get_formatted_address(geo_data)
+            _iss_position['address'] = address
+            return _iss_position
+    return {}
+
+
+def main() -> None:
+    iss_pos = iss_position()
+    logging.info(f"{iss_pos['timestamp']};{iss_pos['latitude']},{iss_pos['longitude']};{iss_pos['address']}")
