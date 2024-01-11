de34eff9a18fb147789dfe2fd2283a9954c9e47b
Jason Armstrong <jasnarmstrng@gmail.com>
2024-01-10 21:50:44 -0400

    feat: Add new API for ISS data
    The data from new API is a dictionary that I only need to add the rever-
    se geolacated address to.
    
    All conversions to human readable formats for e.g. the unix timestamp to
    UTC will be done for frontend. Data to go in database as is
    
    Remove `parse_iss_data`, `ISS_NOW_URL`, and writing to CSV for now. Next
    steps include writing data to database.

--- iss_tracker.py
+++ iss_tracker.py
@@ -2,11 +2,12 @@ import json
 import logging
 import requests
 from datetime import datetime
+from rich import print as pprint
 
 from utils import config
 
 
-ISS_NOW_URL = config["Open-Notify"]["url"]
+WIS_ISS_URL = config["Where-is-ISS"]["url"]
 REV_GEO_URL = config["Geoapify"]["url"]
 REV_GEO_KEY = config["Geoapify"]["key"]
 LOG_FILE = config["Paths"]["log"]
@@ -40,28 +41,13 @@ def unixtime_to_date(timestamp: int) -> str:
     return datetime.utcfromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S+00:00 (UTC)")
 
 
-def parse_iss_data(data: json) -> dict:
-    """Extracts and returns ISS position (lat, long) and timestamp"""
-    try:
-        timestamp = data["timestamp"]
-        position = data["iss_position"]
-        return {
-            'timestamp': unixtime_to_date(timestamp),
-            'latitude': position["latitude"],
-            'longitude': position["longitude"]
-        }
-    except KeyError:
-        logging.error("Invalid ISS data format")
-        return {}
-
-
 def reverse_geolocate(lat: str, long: str) -> json:
     """Return reverse geolocation details"""
     URL = f"{REV_GEO_URL}?lat={lat}&lon={long}&apiKey={REV_GEO_KEY}"
     return get_data_from_api(URL)
 
 
-def get_formatted_address(data: json) -> str:
+def reverse_geolocated_address(data: json) -> str:
     """Extracts and returns formatted address from geolocation data"""
     try:
         return data["features"][0]["properties"]["formatted"]
@@ -70,23 +56,16 @@ def get_formatted_address(data: json) -> str:
         return ""
 
 
-def iss_position() -> None:
-    iss_data = get_data_from_api(ISS_NOW_URL)
+def transform_iss_data() -> None:
+    iss_data = get_data_from_api(WIS_ISS_URL)
     if iss_data:
-        _iss_position = parse_iss_data(iss_data)
-        if iss_position:
-            geo_data = reverse_geolocate(_iss_position['latitude'], _iss_position['longitude'])
-            address = get_formatted_address(geo_data)
-            _iss_position['address'] = address
-            return _iss_position
+        geo_data = reverse_geolocate(iss_data['latitude'], iss_data['longitude'])
+        address = reverse_geolocated_address(geo_data)
+        iss_data['address'] = address
+        return iss_data
     return {}
 
 
 def main() -> None:
-    iss_pos = iss_position()
-
-    with open(LOCATIONS_FILE, "a") as file:
-        if iss_pos:
-            file.write(f"{iss_pos['timestamp']};ISS;{iss_pos['latitude']},{iss_pos['longitude']};{iss_pos['address']}\n")
-        else:
-            logging.info("No data")
+    iss_data = transform_iss_data()
+    pprint(iss_data)
